ec89f73843a8e4451a723f6da15243073af9cd86
NilsonKr <nuotrok444@gmail.com>
2021-06-05 18:09:31 -0500

    :sparkles: [ADD] Delete user-movie auth with cookies

--- services/userMovies.js
+++ services/userMovies.js
@@ -32,6 +32,27 @@ function userMoviesRoutes(app) {
 			next(error);
 		}
 	});
+
+	router.delete('/userMovies/:userMovieId', async (req, res, next) => {
+		const { userMovieId } = req.params;
+		const { token } = req.cookies;
+
+		try {
+			const { status, data } = await axios({
+				method: 'delete',
+				headers: { Authorization: `Bearer ${token}` },
+				url: `${config.apiUrl}/api/userMovies/${userMovieId}`,
+			});
+
+			if (status !== 200) {
+				next(boom.badRequest());
+			}
+
+			res.status(200).json(data);
+		} catch (error) {
+			next(error);
+		}
+	});
 }
 
 module.exports = userMoviesRoutes;
