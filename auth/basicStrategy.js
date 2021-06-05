30bd4710fc55f3b4ffd648612d12bcfecb983c31
NilsonKr <nuotrok444@gmail.com>
2021-06-05 16:16:02 -0500

    :sparkles: [ADD] Sign-in with basic strategy

---
+++ auth/basicStrategy.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
+const axios = require('axios');
+
+const config = require('../config/index');
+const boom = require('@hapi/boom');
+
+passport.use(
+	new BasicStrategy(async (email, password, cb) => {
+		try {
+			const { data, status } = await axios({
+				method: 'post',
+				url: `${config.apiUrl}/api/auth/sign-in`,
+				auth: {
+					password: password,
+					username: email,
+				},
+				data: {
+					apiToken: config.apiToken,
+				},
+			});
+
+			if (!data || status !== 200) {
+				return cb(boom.unauthorized(), false);
+			}
+
+			return cb(false, data);
+		} catch (error) {
+			return cb(error, false);
+		}
+	})
+);
--- server.js
+++ server.js
@@ -2,11 +2,36 @@ const express = require('express');
 const passport = require('passport');
 const config = require('./config/index');
 
-console.log(config);
-
 const app = express();
 
-app.post('/auth/sign-in', (req, res, next) => {});
+// Basic Strategy
+require('./auth/basicStrategy');
+
+app.post('/auth/sign-in', (req, res, next) => {
+	passport.authenticate('basic', (error, data) => {
+		if (error) {
+			next(error);
+		}
+		try {
+			const { token, user } = data;
+
+			req.login(user, { session: false }, err => {
+				if (err) {
+					next(err);
+				}
+
+				res.cookie('token', token, {
+					httpOnly: !config.dev,
+					secure: !config.dev,
+				});
+
+				res.send(200).json(user);
+			});
+		} catch (error) {
+			next(error);
+		}
+	})(req, res, next);
+});
 
 app.post('/auth/sign-up', (req, res, next) => {});
 
